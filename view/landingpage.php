9cf889d6c97f29b268464a2786c7506f196ebad5
Amir Bonvin <amir@Amirs-MacBook-Pro.local>
2019-01-23 14:52:16 +0100

    landing page designed for desktop without responsive

--- view/landingpage.php
+++ view/landingpage.php
@@ -3,10 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/landingpage.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Amir Bonvin</title>
 </head>
 <body>
-    
+    <header>
+        <h4>Amir Bonvin</h4>
+        <img src="../public/images/logoSIT.png" alt="logo de la société Swiss-ITech" id='logoSIT'>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Portfolio</li>
+                <li>Partenaires</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+    <section class='introduction'>
+    <div class='networks'>
+            <img src="../public/images/github-logo.png" alt="lien vers mon github">
+            <img src="../public/images/linkedin-logo.png" alt="lien vers mon linkedin">
+        </div>
+        <div class='main-wrapper'>
+            <div class='photography'><img src="../public/images/cesare.jpeg" alt="me,myself and I"></div>
+            <div class='name_absolute'>
+                <h5>Junior Full-Stack Developer</h5>
+                <h2>Amir Bonvin</h2>
+            </div>
+            <div class='aboutme'>
+                <div class='about'>
+                    <h6>About</h6>
+                    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+            </div>
+                <div class='technologies'>
+                    <h6>Technologies</h6>
+                    <p>HTML5</p>
+                    <p>CSS3</p>
+                    <p>JavaScript</p>
+                    <p>PHP</p>
+                <!--   <p>React/React-Native</p> -->
+                </div>
+                <div class='interests'>
+                    <h6>Contact me for:</h6>
+                    <p>Web Apps</p>
+                    <p>Web Design</p>
+                <!--    <p>Smartphone Applications</p> -->
+                </div>
+            </div>  
+        </div>
+        <div class='front'>
+            <h4>Creative Developer</h4>
+        </div>
+
+
+    </section>
 </body>
 </html>
